3cd6fa20edff23ea42f50efefdfaadedd90645a6
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2025-05-30 11:20:12 +1200

    feat: Добавляет в Placeholder вид изображения

--- src/app/pages/index/index.vue
+++ src/app/pages/index/index.vue
@@ -6,11 +6,13 @@
     <DialogOverlay />
     <DialogPortal>
       <DialogRoot>
-        <DialogContent class="w-full max-w-[500px]">
+        <DialogContent>
           <DialogHeader>
             <Placeholder class="h-14" />
           </DialogHeader>
-          <Placeholder class="flex h-80" />
+
+          <Placeholder picture />
+
           <DialogFooter>
             <Placeholder class="h-14" />
           </DialogFooter>
@@ -18,22 +20,6 @@
       </DialogRoot>
     </DialogPortal>
   </DialogProvider>
-
-  <div class="p-5">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque laudantium debitis ad consectetur illo. Ad, optio voluptas nulla omnis
-    laborum iusto voluptatem placeat quasi molestiae obcaecati corrupti! Laudantium, excepturi atque. Suscipit dicta ducimus sequi nostrum
-    nemo illo nobis voluptatem facilis dolores, temporibus ullam perspiciatis doloribus aliquid rem, vero possimus eveniet totam quod ipsum.
-    Ipsam facilis dicta sed doloremque iusto eum. Dolores quidem similique doloribus totam debitis sint non repellat, soluta veritatis
-    exercitationem. Sapiente, amet iure possimus optio modi commodi quas laudantium culpa natus, delectus quasi dolor quam est! Repudiandae,
-    repellat. Consectetur autem commodi repellat excepturi quo? Ut, autem doloribus, voluptate minima quam impedit et distinctio quas ex
-    dolorem quasi! Optio deleniti molestiae non quaerat modi est, tenetur quis tempora culpa. Quae sequi officia ullam modi autem eligendi
-    mollitia ad aut voluptatem illo iste voluptatum fuga blanditiis quos laboriosam incidunt, aspernatur nisi, nesciunt veniam eum
-    asperiores ratione dolorem exercitationem. Eos, quod! Modi, aliquam laborum soluta odit, esse, explicabo molestias quibusdam neque
-    magnam dolore ea. Reiciendis minima dolor fugiat praesentium, pariatur illum facere minus autem debitis id nemo doloremque incidunt
-    impedit temporibus. Nihil suscipit obcaecati in deserunt quos quo maiores necessitatibus voluptas, quas excepturi eaque veniam id!
-    Nobis, aliquam ea ipsam ipsa illo officiis harum quae blanditiis, sapiente, distinctio necessitatibus. Vitae, modi! Ratione excepturi
-    impedit natus voluptatibus? Odio ad itaque magni minima, vitae, nemo tempore maxime velit debitis, optio amet. Illum architecto nihil
-  </div>
 </template>
 
 <style lang="scss"></style>
